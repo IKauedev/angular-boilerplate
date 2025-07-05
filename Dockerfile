9a85d40714e12b49cb7cf330afc4dfb62a1aed44
Kauê Matos <98132837+IKauedev@users.noreply.github.com>
2025-07-04 22:14:31 -0300

    feat: add sidebar and shell components for improved navigation
    - Implemented SidebarComponent for navigation with dynamic menu items.
    - Created ShellComponent to serve as the main layout wrapper.
    - Introduced ShellService to manage navigation state and permissions.
    - Added ThemeService for managing light and dark themes.
    - Updated environment files to include versioning and language support.
    - Created SCSS files for theming and layout styles.
    - Added translation files for multiple languages (DE, EN, ES, FR, IT).
    - Set up TypeScript configuration for the application.

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:20-alpine as builder
+
+WORKDIR /app
+
+RUN npm i -g pnpm
+
+COPY . .
+
+RUN --mount=type=cache,id=pnpm-store,target=/root/.pnpm-store \
+    pnpm install --frozen-lockfile && \
+    pnpm build
+
+FROM nginx:stable-alpine as runtime
+
+COPY --from=builder /app/dist/angular-boilerplate /usr/share/nginx/html
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
---
+++ src/app/pages/users/list/list.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { UseRandomUser } from '@core/usecases';
+import { RandomUserEntity } from '@core/entities';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
+  standalone: false,
+})
+export class ListComponent implements OnInit {
+  users: RandomUserEntity[] = [];
+  isLoading = true;
+
+  private readonly _useRandomUser = new UseRandomUser();
+
+  ngOnInit() {
+    this._useRandomUser.getAllUsers().subscribe({
+      next: (users) => {
+        this.users = users;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+}
